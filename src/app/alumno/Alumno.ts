f49ca9709ed721d07f2b9b6572aa787dce45ca1b
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2023-09-21 18:02:01 -0300

    agregué más alumnos

--- src/app/alumno/Alumno.ts
+++ src/app/alumno/Alumno.ts
@@ -11,13 +11,3 @@ export class Alumno implements IAlumno{
     }
 
 }
-
-/* ----- INSTANCIAS DE ALUMNO ----- */
-let alumno1 = new Alumno("36720239", "Lucrecia", "Guisasola", new Date(1992, 9, 18), 9.98)
-let alumno2 = new Alumno("11111111", "Clark", "Kent", new Date(1987, 1, 1), 4)
-let alumno3 = new Alumno("22222222", "Bruce", "Wayne", new Date(1990, 4, 25), 10)
-let alumno4 = new Alumno("33333333", "Señorita", "Miel", new Date(1997, 11, 7), 5.5)
-
-let alumnosList: Array<Alumno> = [];
-
-alumnosList.push(alumno1, alumno2, alumno3, alumno4)
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,18 +8,21 @@ import { Alumno } from '../alumno/Alumno';
 })
 export class ContentComponent {
 
-  alumno1 = new Alumno("36720239", "Lucrecia", "Guisasola", new Date(1992, 9, 18), 9.98);
-  alumno2 = new Alumno("11111111", "Clark", "Kent", new Date(1987, 1, 1), 4);
-  alumno3 = new Alumno("22222222", "Bruce", "Wayne", new Date(1990, 4, 25), 10);
-  alumno4 = new Alumno("33333333", "Señorita", "Miel", new Date(1997, 11, 7), 5.5);
+  alumno1 = new Alumno("36720239", "Lucrecia", "Guisasola", new Date(1992, 8, 18), 9.98);
+  alumno2 = new Alumno("11111111", "CLARK", "KENT", new Date(1987, 0, 1), 10);
+  alumno3 = new Alumno("22222222", "bruce", "wayne", new Date(1990, 3, 25), 4);
+  alumno4 = new Alumno("33333333", "seÑoriTA", "MiEl", new Date(1999, 10, 7), 6.5);
+  alumno5 = new Alumno("44444444", "tony", "stark", new Date(1989, 6, 17), 5.5);
+  alumno6 = new Alumno("55555555", "PETER", "PARKER", new Date(2003, 2, 30), 7.75);
+
 
   alumnosList: Array<Alumno> = []
 
   pass: number = 6;
   show: boolean = true;
-  
-  constructor(){
-    this.alumnosList.push(this.alumno1, this.alumno2, this.alumno3, this.alumno4)
+
+  constructor() {
+    this.alumnosList.push(this.alumno1, this.alumno2, this.alumno3, this.alumno4, this.alumno5, this.alumno6)
   }
 
 
