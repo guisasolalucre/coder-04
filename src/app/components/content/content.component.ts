5a8a579b2e22bfc8619dbaa4edd1dda224e9ecd9
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2023-09-27 12:27:00 -0300

    cambie los datos que estaban hardcodeados en el content-component y los pase a una 'falsa api' en data.ts

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Alumno } from '../../model';
+import { alumnos } from '../../data/data'
 
 @Component({
   selector: 'app-content',
@@ -9,33 +10,19 @@ import { Alumno } from '../../model';
 export class ContentComponent {
 
   public readonly passNote: number = 6;
-  public alumnosList: Array<Alumno> = [];
+  public alumnosList: Array<Alumno> = alumnos;
   public list: Array<Alumno> = [];
   public passFlag: number = 0;
   public mostrarTodos: boolean = true;
 
   constructor() { }
 
-  ngOnInit() {
-
-    let alumno1 = new Alumno("36720239", "Lucrecia", "Guisasola", new Date(1992, 8, 18), 9.98);
-    let alumno2 = new Alumno("11111111", "CLARK", "KENT", new Date(1987, 0, 1), 10);
-    let alumno3 = new Alumno("22222222", "bruce", "wayne", new Date(1990, 3, 25), 4);
-    let alumno4 = new Alumno("33333333", "seÑoriTA", "MiEl", new Date(1999, 10, 7), 6.5);
-    let alumno5 = new Alumno("44444444", "tony", "stark", new Date(1989, 6, 17), 5.5);
-    let alumno6 = new Alumno("55555555", "PETER", "PARKER", new Date(2003, 2, 30), 7.75);
-
-    this.alumnosList.push(alumno1, alumno2, alumno3, alumno4, alumno5, alumno6)
-
-    console.log(this.alumnosList)
-  }
-
   getAprobados(): Array<Alumno> {
-    return this.alumnosList.filter((alumno) => alumno.avgNote >= this.passNote)
+    return this.alumnosList.filter((alumno: Alumno) => alumno.avgNote >= this.passNote)
   }
 
   getDesaprobados(): Array<Alumno> {
-    return this.alumnosList.filter((alumno) => alumno.avgNote < this.passNote)
+    return this.alumnosList.filter((alumno: Alumno) => alumno.avgNote < this.passNote)
   }
 
   showList(): Array<Alumno> {
---
+++ src/app/data/data.ts
@@ -0,0 +1,67 @@
+import { Alumno } from "../model";
+
+export let alumnos:Array<Alumno> = [
+    {
+        id: "12345678",
+        name: "John",
+        surname: "Smith",
+        dob: new Date("1990-01-01"),
+        avgNote: 9.5
+    },
+    {
+        id: "87654321",
+        name: "Jane",
+        surname: "Doe",
+        dob: new Date("1995-05-10"),
+        avgNote: 8.2
+    },
+    {
+        id: "98765432",
+        name: "Michael",
+        surname: "Johnson",
+        dob: new Date("1993-10-15"),
+        avgNote: 4
+    },
+    {
+        id: "23456789",
+        name: "Emily",
+        surname: "Brown",
+        dob: new Date("1992-06-20"),
+        avgNote: 6
+    },
+    {
+        id: "76543210",
+        name: "David",
+        surname: "Wilson",
+        dob: new Date("1991-02-25"),
+        avgNote: 5.9
+    },
+    {
+        id: "54321098",
+        name: "Sarah",
+        surname: "Taylor",
+        dob: new Date("1994-08-05"),
+        avgNote: 9.1
+    },
+    {
+        id: "89012345",
+        name: "Daniel",
+        surname: "Anderson",
+        dob: new Date("1996-12-30"),
+        avgNote: 7.3
+    },
+    {
+        id: "45678901",
+        name: "Sophia",
+        surname: "Clark",
+        dob: new Date("1997-04-12"),
+        avgNote: 5.5
+    },
+    {
+        id: "67890123",
+        name: "Matthew",
+        surname: "Robinson",
+        dob: new Date("1998-08-18"),
+        avgNote: 6.5
+    }
+]
